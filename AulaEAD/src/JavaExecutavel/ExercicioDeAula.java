ef098b1b2a8592907a974d2f45c3e9fac931f33c
USUARIO <USUARIO@LAPTOP-4TC5C3VJ>
2023-02-28 14:59:10 -0300

    codigo da tarefa 1 da branch 1

--- AulaEAD/src/JavaExecutavel/ExercicioDeAula.java
+++ AulaEAD/src/JavaExecutavel/ExercicioDeAula.java
@@ -4,7 +4,7 @@ public class ExercicioDeAula {
 
 	public static void main(String[] args) {
 		
-		System.out.print("testando github");
+		System.out.println("Testando o Git-Hub");
 		
 		double x = 42.45;
 		double y = 15.45;
